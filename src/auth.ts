97e470871565f5221403c75be316669380304590
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-12-28 19:50:14 +0530

    Initial repo setup

---
+++ src/auth.ts
@@ -0,0 +1,13 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { prisma } from "@/db/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/dashboard",
+    signOut: "/",
+  },
+});
